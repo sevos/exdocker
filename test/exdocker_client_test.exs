fca9460d1657ef0b1e820ac6542a1e6c2bf138b4
Artur Roszczyk <artur.roszczyk@gmail.com>
2015-10-22 01:19:59 +0200

    Add another test case

--- test/exdocker_client_test.exs
+++ test/exdocker_client_test.exs
@@ -15,4 +15,19 @@ defmodule EXDockerClientTest do
       assert called HTTPotion.get("http://127.0.0.1:2375/v1.20/containers/json")
     end
   end
+
+  test "containers / return single container" do
+    with_mock HTTPotion, [get: fn(_url) ->
+                           %HTTPotion.Response{body: docker_containers_response_with(:redis)}
+                         end] do
+
+      # TODO: assert a map with few keys
+      # assert [{_}] == EXDockerClient.containers(docker_connection)
+      assert called HTTPotion.get("http://127.0.0.1:2375/v1.20/containers/json")
+    end
+  end
+
+  defp docker_containers_response_with(:redis) do
+    "[{\"Id\":\"a1023058c9bbd44e679632e72eb345c2ca169b87061c802ea78b0c295de975f5\",\"Names\":[\"/some-redis\"],\"Image\":\"redis\",\"Command\":\"/entrypoint.sh redis-server\",\"Created\":1445469224,\"Ports\":[{\"PrivatePort\":6379,\"Type\":\"tcp\"}],\"Labels\":{},\"Status\":\"Up 40 seconds\",\"HostConfig\":{\"NetworkMode\":\"default\"}}]\n"
+  end
 end
